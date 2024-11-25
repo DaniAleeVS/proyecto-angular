525edc2a984aee401e7a07a776ad029c98d13bb2
Eder Cornejo Diaz <eder.cornejo@softit.pe>
2024-11-24 22:51:22 -0500

    fin de sec 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+ public title:string = 'Hola Mundo';
+ public counter:number= 19;
+
+
+ increaseBy(value: number):void{
+  this.counter += value;
+
+ }
+
+  ResetCounter(){
+    this.counter= 19
+
+  }
+
+
 }
