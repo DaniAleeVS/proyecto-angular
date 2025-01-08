1708d4185356b920aa0b5fa318f04f672c73b606
Ale <user@users-MacBook-Pro.local>
2025-01-08 01:07:01 -0500

    feat: add main component

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { Component, OnInit,  } from "@angular/core";
 import { Character } from '../interfaces/character.interface';
-=======
-import { Component,  } from "@angular/core";
-import { Character } from "../interfaces/character.interface";
->>>>>>> d748257828d0a11d500367900b08a6197e859cb0
 
 @Component({
   selector: 'dbz-main-page',
@@ -25,14 +20,12 @@ import { Character } from "../interfaces/character.interface";
       name: 'Bills',
       power: 50000,
     }
-<<<<<<< HEAD
-
   ]
   onNewCharacter(characters: Character):void {
  this.characters.push(characters);
-
   }
+onDeleteCharacter( index: number){
+  this.characters.splice(index)
 }
-=======
-  ]}
->>>>>>> d748257828d0a11d500367900b08a6197e859cb0
+
+}
